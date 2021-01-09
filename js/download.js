e70de2d3239cdd43506078c985e692ebc1ee050e
ErikBoesen <me@erikboesen.com>
2021-01-09 18:34:58 -0500

    Add download script

---
+++ js/download.js
@@ -0,0 +1,28 @@
+function getOS() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
+
+let os = getOS();
+if (os === "Android") {
+    window.location.replace("https://play.google.com/store/apps/details?id=com.adisa.diningplus");
+} else if (os === "iOS") {
+    window.location.replace("https://apps.apple.com/us/app/yale-menus/id1273168650");
+} else {
+    window.location.replace("/");
+}
